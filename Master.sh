6738b2821ae777e0e8913c9b0a2492696142a62c
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-21 18:38:44 +0530

    Adding Master.sh

--- Master.sh
+++ Master.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+USER=$(id -u)
+if [ $USER -ne 0 ]
+then
+echo "Please run the script with root access"
+else
+echo "You are a root user"
+fi
 time=$(date +%F-%H-%M-%S)
 script=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$time-$script.log
 validate() {
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
     echo "$2....Failure"
     else
     echo "$1...success"
+    fi
 }
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
