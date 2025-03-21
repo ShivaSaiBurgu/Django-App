3e8502f8bc82fd058198ad0f9ceb7065c08b8ffc
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-21 18:48:08 +0530

    Adding Master.sh

--- Master.sh
+++ Master.sh
@@ -18,23 +18,23 @@ validate() {
     fi
 }
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
- https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-validate $? "Downloading libraries"
+ https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key &>>$LOGFILE
+validate $? "Downloading libraries" 
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  /etc/apt/sources.list.d/jenkins.list > /dev/null &>>$LOGFILE
   validate $? "echoing"
-  sudo apt-get update
+  sudo apt-get update &>>$LOGFILE
    validate $? "updating packages"
-  sudo apt-get install jenkins
+  sudo apt-get install jenkins &>>$LOGFILE
    validate $? "Installing Jenkins"
-  sudo apt update
+  sudo apt update &>>$LOGFILE
   validate $? "updating packages"
-  sudo apt install fontconfig openjdk-17-jre
+  sudo apt install fontconfig openjdk-17-jre &>>$LOGFILE
    validate $? "Installing Java"
   java -version
-  sudo systemctl enable jenkins
+  sudo systemctl enable jenkins &>>$LOGFILE
    validate $? "enabling jenkins"
-  sudo systemctl start jenkins
+  sudo systemctl start jenkins &>>$LOGFILE
    validate $? "starting jenkins"
   sudo systemctl status jenkins
