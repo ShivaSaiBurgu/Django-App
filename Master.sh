d11a7592b9bbe4f6006107c3c15085ff6d86254b
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-21 19:29:59 +0530

    Adding Master.sh

--- Master.sh
+++ Master.sh
@@ -32,7 +32,6 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
 VALIDATE $? "updating packages"
   sudo apt-get install jenkins &>>$LOGFILE
    VALIDATE $? "Installing Jenkins"
-  sudo apt update &>>$LOGFILE
  VALIDATE $? "updating packages"
   sudo apt install fontconfig openjdk-17-jre &>>$LOGFILE
    VALIDATE $? "Installing Java"
